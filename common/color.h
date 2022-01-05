9f103193ffd124d7c6571bf80211a4f38d451e31
LeoProXXX <michal3184@vp.pl>
2022-01-05 22:27:46 +0100

    feat(): handle input arguments (#3)
    Co-authored-by: Michal Zebrowski <michal.zebrowski2.stud@pw.edu.pl>

--- common/color.h
+++ common/color.h
@@ -6,7 +6,7 @@
 #include <iostream>
 
 
-void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+void write_color(std::ostream &out, color pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
@@ -15,12 +15,6 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     if (r != r) r = 1.0;
     if (g != g) g = 1.0;
     if (b != b) b = 1.0;
-
-    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
-    auto scale = 1.0 / samples_per_pixel;
-    // r = sqrt(scale * r);
-    // g = sqrt(scale * g);
-    // b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
