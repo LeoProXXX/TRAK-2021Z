c278df0cf413485ada41b600c627132edd9bce35
Patrycja <patrycja.wawrzyn@gmail.com>
2022-01-10 19:12:51 +0100

    add bvh

---
+++ bvh.h
@@ -0,0 +1,13 @@
+#ifndef BVH_H
+#define BVH_H
+
+#include "common/helpers.h"
+#include "common/mesh.h"
+#include "acceleration_structure.h"
+
+class BVH : public AccelerationStructure
+{
+
+};
+
+#endif
